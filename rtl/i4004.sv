5b96373b2e46f862c73e9318ceff88fcf2f21c88
Alejandro Argueta <alejandro@argueta.xyz>
2019-07-22 23:47:27 -0500

    Barebones OPR Decoding
    Decode the OPR nibble and not much else. Simulates with a bunch of warnings.

--- rtl/i4004.sv
+++ rtl/i4004.sv
@@ -1,3 +1,4 @@
+/* verilator lint_off UNUSED */
 module i4004 (
   input         clk,
   input         rst,
@@ -28,32 +29,275 @@ always @(posedge clk) begin
 end
 
 // Address Register
-mcs4::addr_t stack [3:0];
-logic [1:0] pc_ptr;
-// TODO: Addr incrementer
-mcs4::byte_t addr_buff [2:0];
-mcs4::instr_t inst;
+// [ADDRESS REGISTER 4x12b DRAM]
+mcs4::addr_t [3:0] stack;
+logic        [1:0] stack_ptr;
+mcs4::addr_t       pc, next_pc;
+logic stack_push, stack_pop;
+// Stack logic
+always @(posedge clk) begin
+  if(rst) begin
+    stack_ptr <= 0;
+    pc <= 0;
+  end else if(stack_push) begin
+    stack[stack_ptr] <= pc;
+    stack_ptr <= stack_ptr + 1;
+    pc <= next_pc;
+  end else if(stack_pop) begin
+    stack_ptr <= stack_ptr - 1;
+    pc <= stack[stack_ptr - 2'd1];
+  end else begin
+    stack[stack_ptr] <= next_pc;
+    pc <= next_pc;
+  end
+end
+
+// Next address selection
+// [DECODER DRIVER & MUX]
+logic [2:0] next_addr_mode;
+always @(posedge clk) begin
+  if(rst) begin
+    next_pc <= 0;
+  end else begin
+    case (next_addr_mode)
+      3'd0: next_pc <= addr_incr;
+      default : next_pc <= addr_incr;
+    endcase
+  end
+end
+
+// Address incrementer
+mcs4::char_t [2:0] addr_buff;
+mcs4::char_t [2:0] addr_incr;
+logic        [2:0] addr_carry;
+logic              addr_overflow;
+always_ff @(posedge clk) begin
+  if(rst) begin
+    addr_incr[0] <= 0;
+    addr_incr[1] <= 0;
+    addr_incr[2] <= 0;
+    addr_carry <= '0;
+  end else begin
+    // Spec calls for lookahead adder, whoops
+    {addr_carry[0], addr_incr[0]} <= addr_buff[0] + 1;
+    {addr_carry[1], addr_incr[1]} <= addr_buff[1] + addr_carry[0];
+    {addr_carry[2], addr_incr[2]} <= addr_buff[2] + addr_carry[1];
+  end
+end
+assign addr_overflow = addr_carry[2];
+
+// Index Register
+mcs4::char_t [mcs4::Num_reg_pairs-1:0][1:0] index_register;
+mcs4::char_t [1:0] irp_wbuf;
+mcs4::char_t [1:0] irp_rbuf;
+mcs4::raddr_t      ir_addr;
+mcs4::rpaddr_t     irp_addr;
+logic pair_mode, ir_wen;
+always @(posedge clk) begin : proc_index_register
+  if(ir_wen) begin
+    if(pair_mode) begin
+      index_register[irp_addr] <= irp_wbuf;
+    end else begin
+      index_register[irp_addr][ir_addr[0]] <= irp_wbuf[0];
+    end
+  end else begin
+    irp_rbuf <= index_register[irp_addr];
+  end
+end
+
+// Adder
+mcs4::char_t adb_buf;
+mcs4::char_t accum;
+logic        carry;
+logic [2:0]  cm_ctl;
+logic [2:0]  accum_ctl;
+always_ff @(posedge clk) begin : proc_accum
+  if(rst) begin
+    accum <= 0;
+  end else begin
+    // case (accum_ctl)
+    //   ACCUM: {carry, accum} <= accum + adb_buf + carry;
+    //   LROT:  accum <= accum << 1;
+    //   RROT:  accum <= accum >> 1;
+    //   default :   ;
+    // endcase
+  end
+end
+
+// Instruction Register Decode & Control
+mcs4::instr_t [1:0] instr;
+logic         double_instr;
+logic         is_instr2;
+
+mcs4::raddr_t      ird_reg;
+mcs4::rpaddr_t     ird_reg_pair;
+mcs4::char_t [1:0] ird_data;
+mcs4::char_t [2:0] ird_addr;
+mcs4::char_t       ird_cond;
+
+
+always_ff @(posedge clk) begin : proc_instr
+  if(rst) begin
+    instr[0] <= mcs4::NOP;
+  end else begin
+    if(icyc == mcs4::M1) begin
+      instr[double_instr].opr <= bus;
+    end else if(icyc == mcs4::M1) begin
+      instr[double_instr].opa <= bus;
+    end
+  end
+end
+
+// Decode OPR
+mcs4::opchar_type_t opa_type, opr_type;
+logic instr_mod;
+always @(posedge clk) begin : proc_decode_opr
+  if(icyc == mcs4::M2) begin
+    if(!is_instr2) begin
+      case (instr[0].opr)
+        mcs4::NOP : opa_type <= mcs4::NOP_OP;
+        mcs4::JCN : opa_type <= mcs4::COND;
+        mcs4::FIM : opa_type <= mcs4::REG_PR;
+        // mcs4::SRC
+        mcs4::FIN : opa_type <= mcs4::REG_PR;
+        // mcs4::JIN
+        mcs4::JUN : opa_type <= mcs4::ADDR_HI;
+        mcs4::JMS : opa_type <= mcs4::ADDR_HI;
+        mcs4::INC : opa_type <= mcs4::REG;
+        mcs4::ISZ : opa_type <= mcs4::REG;
+        mcs4::ADD : opa_type <= mcs4::REG;
+        mcs4::SUB : opa_type <= mcs4::REG;
+        mcs4::LD  : opa_type <= mcs4::REG;
+        mcs4::XCH : opa_type <= mcs4::REG;
+        mcs4::BBL : opa_type <= mcs4::DATA_LO;
+        mcs4::LDM : opa_type <= mcs4::DATA_LO;
+        mcs4::IORAM_OPR : opa_type <= mcs4::IORAM;
+        mcs4::ACCUM_OPR : opa_type <= mcs4::ACCUM;
+        default : opa_type <= mcs4::NOP_OP;
+      endcase
+    end else begin
+      // Decode DWords based on original OPR
+      case (instr[0].opr)
+        mcs4::JCN : begin
+          opr_type <= mcs4::ADDR_MD;
+          opa_type <= mcs4::ADDR_LO;
+        end
+        mcs4::FIM : begin
+          opr_type <= mcs4::DATA_HI;
+          opa_type <= mcs4::DATA_LO;
+        end
+        mcs4::JUN : begin
+          opr_type <= mcs4::ADDR_MD;
+          opa_type <= mcs4::ADDR_LO;
+        end
+        mcs4::JMS : begin
+          opr_type <= mcs4::ADDR_MD;
+          opa_type <= mcs4::ADDR_LO;
+        end
+        mcs4::ISZ : begin
+          opr_type <= mcs4::ADDR_MD;
+          opa_type <= mcs4::ADDR_LO;
+        end
+        default : begin
+          opr_type <= mcs4::NOP_OP;
+          opa_type <= mcs4::NOP_OP;
+        end
+      endcase
+    end
+  end
+end
+
+always @(posedge clk) begin : proc_decode_opa
+   if(icyc == mcs4::X1) begin
+    case (opa_type)
+      mcs4::REG     : ird_reg <= instr[0].opa;
+      mcs4::REG_PR  : {ird_reg_pair, instr_mod} <= bus; //instr[0].opa;
+      mcs4::DATA_LO : ird_data[0] <= bus; //instr[0].opa;
+      mcs4::ADDR_HI : ird_addr[2] <= bus; //instr[0].opa
+      mcs4::ADDR_LO : ird_addr[0] <= bus; //instr[1].opa
+      mcs4::COND    : ird_cond <= bus; // instr.opa;
+      mcs4::IORAM   : begin
+        case (instr[0].opa)
+
+          default : begin
+            /* default */;
+          end
+        endcase
+      end
+      mcs4::ACCUM   : ;
+      default : begin
+        /* default */;
+      end
+    endcase
+    case (opr_type)
+      mcs4::ADDR_MD : ird_addr[1] <= instr[1].opr;
+      mcs4::DATA_HI : ird_data[1] <= instr[1].opr;
+      default : ;
+    endcase
+   end
+end
+
+
 
 // Bus arbitrator
-mcs4::byte_t bus;
-logic da_out;
-mcs4::byte_t ram_ctl;
+mcs4::char_t bus;
+mcs4::char_t ram_ctl;
 logic io_read;
-always @(posedge clk) begin
+always_comb begin : proc_bus
   case (icyc)
     mcs4::A1 : bus <= addr_buff[0];
     mcs4::A2 : bus <= addr_buff[1];
     mcs4::A3 : bus <= addr_buff[2];
-    mcs4::M1 : bus <= inst.opr;
-    mcs4::M2 : bus <= inst.opa;
+    mcs4::M1 : bus <= dbus_in; //instr.opr;
+    mcs4::M2 : bus <= dbus_in; //instr.opa;
     mcs4::X1 : bus <= '0;
     mcs4::X2 : bus <= io_read? ram_ctl : dbus_in;
     mcs4::X3 : bus <= ram_ctl;
-    default : bus <= addr_buff[0];
+    default  : bus <= addr_buff[0];
   endcase // icyc
 end
 
+// Save dbus_in values
+always @(posedge clk) begin
+  case (icyc)
+    // mcs4::A1 : dbus_out <= addr_buff[0];
+    // mcs4::A2 : dbus_out <= addr_buff[1];
+    // mcs4::A3 : dbus_out <= addr_buff[2];
+    mcs4::M1 : instr[is_instr2].opr <= dbus_in;
+    mcs4::M2 : instr[is_instr2].opa <= dbus_in;
+    // mcs4::X1 : bus <= '0;
+    // mcs4::X2 : bus <= io_read? ram_ctl : dbus_in;
+    // mcs4::X3 : bus <= ram_ctl;
+    default : ;
+  endcase // icyc
 
+  // if(mcs4::A1) begin
+  //   addr_buff[0] <= dbus_in;
+  // end
+  // if(mcs4::A2) begin
+  //   addr_buff[1] <= dbus_in;
+  // end
+  // if(mcs4::A3) begin
+  //   addr_buff[2] <= dbus_in;
+  // end
+  // if(icyc == mcs4::M1) begin
+  //   instr[is_instr2].opr <= dbus_in;
+  // end
+  // if(icyc == mcs4::M2) begin
+  //   instr[is_instr2].opa <= dbus_in;
+  // end
+end
+
+
+// Lint unused
+assign stack_ptr = '0;
+assign dbus_out = bus;
+assign cm_ram = '0;
+assign cm_rom = '0;
+assign stack = '0;
+assign addr_buff = '0;
+assign ram_ctl = '0;
+assign io_read = '0;
 
 // Index Register
 
@@ -61,3 +305,4 @@ end
 
 
 endmodule
+/* verilator lint_on UNUSED */
--- rtl/mcs4_pkg.sv
+++ rtl/mcs4_pkg.sv
@@ -1,7 +1,7 @@
 //  mcs4_pkg.sv
-
+/* verilator lint_off DECLFILENAME */
 package mcs4;
-
+/* verilator lint_on DECLFILENAME */
   // Parameters ...........................................................//
   localparam Cycles_per_instruction = 8;
 
@@ -18,7 +18,7 @@ package mcs4;
 
   // ROM
   localparam Bytes_per_page   = 256;
-  localparam Bytes_per_rom    = 2048;
+  localparam Bytes_per_rom    = 4096;
   localparam Pages_per_rom    = Bytes_per_rom / Bytes_per_page;
   localparam Page_index_width = $clog2(Pages_per_rom);
 
@@ -30,13 +30,9 @@ package mcs4;
   localparam Ram_status_per_reg = 4;
 
   // Data Types ...........................................................//
-  typedef logic [4-1:0] char_t;
-  typedef logic [8-1:0] byte_t;
-  typedef logic [12-1:0] addr_t;
-  // Fuck iVerilog
-  // typedef logic [Char_width-1:0] char_t;
-  // typedef logic [Byte_width-1:0] byte_t;
-  // typedef logic [Addr_width-1:0] addr_t;
+  typedef logic [Char_width-1:0] char_t;
+  typedef logic [Byte_width-1:0] byte_t;
+  typedef logic [Addr_width-1:0] addr_t;
 
   // Register File
   typedef logic [Reg_addr_width-1:0] raddr_t;
@@ -56,88 +52,104 @@ package mcs4;
     ram_char_sel_t rchar;
   } ram_addr_t;
 
+  //*************************
+  // CPU SPECIFIC
+  //*************************
   // Opcodes
   typedef struct packed {
     char_t opr;
     char_t opa;
   } instr_t;
 
-  typedef struct packed {
-    char_t op_b;
-    char_t op_c;
-  } instr2_t;
+  typedef enum logic [3:0] {
+    NOP_OP   = 4'd0,
+    REG      = 4'd1,
+    REG_PR   = 4'd2,
+    DATA_HI  = 4'd3,
+    DATA_LO  = 4'd4,
+    ADDR_HI  = 4'd5,
+    ADDR_MD  = 4'd6,
+    ADDR_LO  = 4'd7,
+    COND     = 4'd8,
+    IORAM    = 4'd9,
+    ACCUM    = 4'd10
+  } opchar_type_t;
 
-  // Machine
-  localparam NOP = 8'b0000_0000; // No Operation
-  localparam JCN = 8'b0001_0000; // Jump Conditional
-  localparam FIM = 8'b0010_0000; // Fetch Immediate
-  localparam SRC = 8'b0010_0001; // Send Register Control
-  localparam FIN = 8'b0011_0000; // Fetch Indirect
-  localparam JIN = 8'b0011_0001; // Jump Indirect
-  localparam JUN = 8'b0100_0000; // Jump Uncoditional
-  localparam JMS = 8'b0101_0000; // Jump to Subroutine
-  localparam INC = 8'b0110_0000; // Increment
-  localparam ISZ = 8'b0111_0000; // Increment and Skip
-  localparam ADD = 8'b1000_0000; // Add
-  localparam SUB = 8'b1001_0000; // Subtract
-  localparam LD  = 8'b1010_0000; // Load
-  localparam XCH = 8'b1011_0000; // Exchange
-  localparam BBL = 8'b1100_0000; // Branch Back and Load
-  localparam LDM = 8'b1101_0000; // Load Immediate
-  // I/O and RAM
-  localparam WRM = 8'b1110_0000; // Write Main Memory
-  localparam WMP = 8'b1110_0001; // Write RAM Port
-  localparam WRR = 8'b1110_0010; // Write ROM Port
-  localparam WR0 = 8'b1110_0100; // Write Status Char 0
-  localparam WR1 = 8'b1110_0101; // Write Status Char 1
-  localparam WR2 = 8'b1110_0110; // Write Status Char 2
-  localparam WR3 = 8'b1110_0111; // Write Status Char 3
-  localparam SBM = 8'b1110_1000; // Subtract Main Memory
-  localparam RDM = 8'b1110_1001; // Read Main Memory
-  localparam RDR = 8'b1110_1010; // Read ROM Port
-  localparam ADM = 8'b1110_1011; // Add Main Memory
-  localparam RD0 = 8'b1110_1100; // Read Status Char 0
-  localparam RD1 = 8'b1110_1101; // Read Status Char 1
-  localparam RD2 = 8'b1110_1110; // Read Status Char 2
-  localparam RD3 = 8'b1110_1111; // Read Status Char 3
-  // Accumulator
-  localparam CLB = 8'b1111_0000; // Clear Both
-  localparam CLC = 8'b1111_0001; // Clear Carry
-  localparam IAC = 8'b1111_0010; // Increment Accumulator
-  localparam CMC = 8'b1111_0011; // Complement Carry
-  localparam CMA = 8'b1111_0100; // Complement
-  localparam RAL = 8'b1111_0101; // Rotate Left
-  localparam RAR = 8'b1111_0110; // Rotate Right
-  localparam TCC = 8'b1111_0111; // Transfer Carry and Clear
-  localparam DAC = 8'b1111_1000; // Decrement Accumulator
-  localparam TCS = 8'b1111_1001; // Transfer Carry Subtract
-  localparam STC = 8'b1111_1010; // Set Carry
-  localparam DAA = 8'b1111_1011; // Decimal Adjust Accumulator
-  localparam KBP = 8'b1111_1100; // Keybord Process
-  localparam DCL = 8'b1111_1101; // Designate Command Line
+  // Machine (OPR's)
+  localparam NOP = 4'b0000; // OPR: 0000 - No Operation
+  localparam JCN = 4'b0001; // OPR: 0000 - Jump Conditional
+  localparam FIM = 4'b0010; // OPR: 0000 - Fetch Immediate
+  localparam SRC = 4'b0010; // OPR: 0001 - Send Register Control
+  localparam FIN = 4'b0011; // OPR: 0000 - Fetch Indirect
+  localparam JIN = 4'b0011; // OPR: 0001 - Jump Indirect
+  localparam JUN = 4'b0100; // OPR: 0000 - Jump Uncoditional
+  localparam JMS = 4'b0101; // OPR: 0000 - Jump to Subroutine
+  localparam INC = 4'b0110; // OPR: 0000 - Increment
+  localparam ISZ = 4'b0111; // OPR: 0000 - Increment and Skip
+  localparam ADD = 4'b1000; // OPR: 0000 - Add
+  localparam SUB = 4'b1001; // OPR: 0000 - Subtract
+  localparam LD  = 4'b1010; // OPR: 0000 - Load
+  localparam XCH = 4'b1011; // OPR: 0000 - Exchange
+  localparam BBL = 4'b1100; // OPR: 0000 - Branch Back and Load
+  localparam LDM = 4'b1101; // OPR: 0000 - Load Immediate
+  localparam IORAM_OPR = 4'b1110;
+  localparam ACCUM_OPR = 4'b1111;
+
+  // I/O and RAM (OPA's)
+  localparam WRM = 4'b0000; // Write Main Memory
+  localparam WMP = 4'b0001; // Write RAM Port
+  localparam WRR = 4'b0010; // Write ROM Port
+  localparam WR0 = 4'b0100; // Write Status Char 0
+  localparam WR1 = 4'b0101; // Write Status Char 1
+  localparam WR2 = 4'b0110; // Write Status Char 2
+  localparam WR3 = 4'b0111; // Write Status Char 3
+  localparam SBM = 4'b1000; // Subtract Main Memory
+  localparam RDM = 4'b1001; // Read Main Memory
+  localparam RDR = 4'b1010; // Read ROM Port
+  localparam ADM = 4'b1011; // Add Main Memory
+  localparam RD0 = 4'b1100; // Read Status Char 0
+  localparam RD1 = 4'b1101; // Read Status Char 1
+  localparam RD2 = 4'b1110; // Read Status Char 2
+  localparam RD3 = 4'b1111; // Read Status Char 3
+
+  // Accumulator (OPA's)
+  localparam CLB = 4'b0000; // Clear Both
+  localparam CLC = 4'b0001; // Clear Carry
+  localparam IAC = 4'b0010; // Increment Accumulator
+  localparam CMC = 4'b0011; // Complement Carry
+  localparam CMA = 4'b0100; // Complement
+  localparam RAL = 4'b0101; // Rotate Left
+  localparam RAR = 4'b0110; // Rotate Right
+  localparam TCC = 4'b0111; // Transfer Carry and Clear
+  localparam DAC = 4'b1000; // Decrement Accumulator
+  localparam TCS = 4'b1001; // Transfer Carry Subtract
+  localparam STC = 4'b1010; // Set Carry
+  localparam DAA = 4'b1011; // Decimal Adjust Accumulator
+  localparam KBP = 4'b1100; // Keybord Process
+  localparam DCL = 4'b1101; // Designate Command Line
 
   /* Opcode Masks for:
       NOP, WRM, WMP, WRR, WR0, WR1, WR2, WR3,
       SBM, RDM, RDR, ADM, RD0, RD1, RD2, RD3,
       CLB, CLC, IAC, CMC, CMA, RAL, RAR, TCC,
       DAC, TCS, STC, DAA, KBP, DCL */
-  localparam Inst_opcode_mask_full = 8'b1111_1111;
+  localparam Instr_opcode_mask_full = 8'b1111_1111;
 
   /* Opcode masks for:
       JCN, JUN, JMS, INC, ISZ, ADD, SUB, LD,
       XCH, BBL, LDM */
-  localparam Inst_opcode_mask_high = 8'b1111_0000;
+  localparam Instr_opcode_mask_high = 8'b1111_0000;
 
   /* Opcode masks for:
       FIM, SRC, FIN, JIN */
-  localparam Inst_opcode_mask_low  = 8'b1111_0001;
+  localparam Instr_opcode_mask_low  = 8'b1111_0001;
 
   // Argument masks
-  localparam Inst_arg_mask_condition = 8'b0000_1111;
-  localparam Inst_arg_mask_addr_high = 8'b0000_1111;
-  localparam Inst_arg_mask_register  = 8'b0000_1111;
-  localparam Inst_arg_mask_reg_pair  = 8'b0000_1110;
-  localparam Inst_arg_mask_data      = 8'b0000_1111;
+  localparam Instr_arg_mask_condition = 8'b0000_1111;
+  localparam Instr_arg_mask_addr_high = 8'b0000_1111;
+  localparam Instr_arg_mask_register  = 8'b0000_1111;
+  localparam Instr_arg_mask_reg_pair  = 8'b0000_1110;
+  localparam Instr_arg_mask_data      = 8'b0000_1111;
 
   typedef enum logic [2:0] {
     A1 = 3'd0,
