69bfe9e0d79cfe71643c76e8c728e5504c76bce7
Alejandro Argueta <alejandro@argueta.xyz>
2019-08-16 14:34:10 -0700

    JIN added & tested

--- roms/stack_test.asm
+++ roms/stack_test.asm
@@ -1,15 +1,19 @@
+; Load hardcoded addr of main and jump indirect
+init    NOP
+        LDM $6
+        XCH R0
+        JIN P0
+        NOP
+        NOP
+
 ; Load 1 and write to Reg0
-init
-  NOP
-  LD  $1
-  XCH R0
+main    LD  $1
+        XCH R0
 
  ; Jump to incr subroutine, then loop back
-loop
-  JMS incr
-  JUN loop
+loop    JMS incr
+        JUN loop
 
 ; Increment Reg0, return 2
-incr
-  INC R0
-  BBL $2
+incr    INC R0
+        BBL $2
