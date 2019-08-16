c7526a4e98692227c0f819dc8ac60ce8dd4f73e9
Alejandro Argueta <alejandro@argueta.xyz>
2019-08-16 00:31:01 -0700

    Comments in stack_test.asm

--- roms/stack_test.asm
+++ roms/stack_test.asm
@@ -1,12 +1,15 @@
+; Load 1 and write to Reg0
 init
   NOP
   LD  $1
   XCH $0
 
+ ; Jump to incr subroutine, then loop back
 loop
   JMS incr
   JUN loop
 
+; Increment Reg0, return 2
 incr
   INC $0
   BBL $2
