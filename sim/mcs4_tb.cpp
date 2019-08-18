803ef671c864b1e86018a0b2cce7af496db0f379
Alejandro Argueta <alejandro@argueta.xyz>
2019-08-18 15:44:41 -0700

    Better HROM printing

--- sim/mcs4_tb.cpp
+++ sim/mcs4_tb.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <stdlib.h>
@@ -19,18 +20,25 @@ vector<int> parseRom(string filename) {
         while (getline(stream, line)) {
             rom_string += line + " ";
         }
-        cerr << "ROM to load:\n" << rom_string << endl;
         stream.close();
 
         istringstream iss(rom_string);
         copy(istream_iterator<string>(iss),
              istream_iterator<string>(),
              back_inserter(rom_byte_strings));
+        cout << "ROM to load:" << endl;
+        int i = 0;
         for (vector<string>::iterator it = rom_byte_strings.begin();
              it != rom_byte_strings.end(); ++it) {
             unsigned int b = stoul(*it, nullptr, 16);
             rom_bytes.push_back(b);
+            if (i % 8 == 0) {
+                cout << "\n0x" << setfill('0') << setw(2) << hex << i << ":\t";
+            }
+            cout << setfill('0') << setw(2) << hex << b << " ";
+            i++;
         }
+        cout << endl;
     } else {
         cerr << "ERROR: Unable to open ROM file \'" << filename <<
                 "\', using linked default.\n" << endl;
@@ -50,7 +58,7 @@ void initMemory(TESTBENCH<Vmcs4_tb>* tb, vector<int> rom_bytes) {
     tb->m_core->dbg_wen = 0;
     tb->tick();
     tb->m_core->rst = 0;
-    cout << "Done initializing " << rom_bytes.size() << " bytes" << endl;
+    cout << "Done initializing " << dec << rom_bytes.size() << " bytes" << endl;
 }
 
 int main(int argc, char **argv, char** env) {
