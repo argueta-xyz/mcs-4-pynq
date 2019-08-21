13e5832a8df2b132a96e122213378a89f7335783
Alejandro Argueta <alejandro@argueta.xyz>
2019-08-21 13:21:23 -0700

    Read 8b arg, decarg subroutine working
    Can now load 8-bits of argument, check that it's not 0-2, and set working set to arg-2

--- roms/fibonacci_rand.asm
+++ roms/fibonacci_rand.asm
@@ -1,9 +1,12 @@
-rdarg	LDM 0
-		XCH R0
-		XCH R1		; R0R1 = 0
+		NOP
+rdarg	FIM R0R1, $00
 		SRC R0R1
 		RDR
-		XCH R4		; Read ROM port 0x0 to R4, R5 is 0x0, so arg = 2*(ROM Input)
+		XCH R5		; Read ROM port [1, 0] -> [R4, R5]
+		FIM R0R1, $10
+		SRC R0R1
+		RDR
+		XCH R4
 
 setup	LDM 0
 		SRC R0R1
@@ -19,41 +22,57 @@ setup	LDM 0
 		LDM 1
 		WRM			; RAM[5:0] = [0x01, 0x01, 0x0]
 
-ckarg0	LD	R4
+ckarg0	LD	R5
 		JCN CNZ	ckarg1
-		LD	R5
+		LD	R4
 		JCN CNZ	ckarg1
 		JUN done		; arg == 0? Done if so
 
 ckarg1	LDM 1
-		SUB	R4
+		SUB	R5
 		JCN CNZ	ckarg2
 		CLB
-		LD	R5
 		LDM 1
-		XCH R0
+		XCH R1			; preload answer in case arg == 1
+		LD	R4
 		JCN CZ	done	; arg == 1? done if so
 
 ckarg2	LDM 2
 		SUB	R4
-		JCN CNZ	fibmn
+		JCN CNZ	cparg
 		CLB
 		LD	R5
 		LDM 1
 		XCH R0
 		JCN CZ	done	; arg == 2? done if so
 
-		LD	R5
+cparg	LD	R5
 		XCH	R7
 		LD	R4
 		XCH	R6	; Copy arg to R6R7
+		JMS decarg
+		JMS decarg
+		JUN fibmn
 
-decr	CLC
+decarg	CLC
 		LDM 1
-		LD	R7
+		XCH R7	; {c,accum} = {0,arg.lo}, R6 = 1
+		SUB R7	; {c,accum} = {b,arg.lo - 1}
+		XCH R7	; {c,accum} = {b,1},      R6 = arg.lo - 1
+		LDM 0	; {c,accum} = {b,0}
+		RAL		; {c,accum} = {0,b}
+		XCH R6  ; accum = arg.hi, R7 = b
+		SUB R6
+		XCH R6  ; accum = b,      R7 = arg.hi - b
+		BBL 0
+
+
 fibmn	NOP
 
-done	NOP
+done	FIM R2R3, $00
+		SRC R2R3
+		LDM $6 ; Write to RAM output port
+		WMP
 
 CZ=%0010
 CNZ=%0011
