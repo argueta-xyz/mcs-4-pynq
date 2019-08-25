24bf261dacfe0c8e639fdeb4e0ab8cd20921fad5
argueta-xyz <alejandro@argueta.xyz>
2019-08-25 16:16:26 -0700

    Check-in Vivado IP wrappers
    MCS-4 wrapper IP's with minimal customization

---
+++ ip_repo/mcs4_1.0/drivers/mcs4_v1_0/src/mcs4.h
@@ -0,0 +1,54 @@
+
+#ifndef MCS4_H
+#define MCS4_H
+
+
+/****************** Include Files ********************/
+#include "xil_types.h"
+#include "xstatus.h"
+
+
+/**************************** Type Definitions *****************************/
+/**
+ *
+ * Write/Read 32 bit value to/from MCS4 user logic memory (BRAM).
+ *
+ * @param   Address is the memory address of the MCS4 device.
+ * @param   Data is the value written to user logic memory.
+ *
+ * @return  The data from the user logic memory.
+ *
+ * @note
+ * C-style signature:
+ * 	void MCS4_mWriteMemory(u32 Address, u32 Data)
+ * 	u32 MCS4_mReadMemory(u32 Address)
+ *
+ */
+#define MCS4_mWriteMemory(Address, Data) \
+    Xil_Out32(Address, (u32)(Data))
+#define MCS4_mReadMemory(Address) \
+    Xil_In32(Address)
+
+/************************** Function Prototypes ****************************/
+/**
+ *
+ * Run a self-test on the driver/device. Note this may be a destructive test if
+ * resets of the device are performed.
+ *
+ * If the hardware system is not built correctly, this function may never
+ * return to the caller.
+ *
+ * @param   baseaddr_p is the base address of the MCS4instance to be worked on.
+ *
+ * @return
+ *
+ *    - XST_SUCCESS   if all self-test code passed
+ *    - XST_FAILURE   if any self-test code failed
+ *
+ * @note    Caching must be turned off for this function to work.
+ * @note    Self test may fail if data memory and device are not on the same bus.
+ *
+ */
+XStatus MCS4_Mem_SelfTest(void * baseaddr_p);
+
+#endif // MCS4_H
---
+++ ip_repo/mcs4_1.0/xgui/mcs4_v1_0.tcl
@@ -0,0 +1,203 @@
+
+# Loading additional proc with user specified bodies to compute parameter values.
+source [file join [file dirname [file dirname [info script]]] gui/mcs4_v1_0.gtcl]
+
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  set C_S_AXI_ADDR_WIDTH [ipgui::add_param $IPINST -name "C_S_AXI_ADDR_WIDTH" -parent ${Page_0}]
+  set_property tooltip {Width of S_AXI address bus} ${C_S_AXI_ADDR_WIDTH}
+  ipgui::add_param $IPINST -name "C_S_AXI_BASEADDR" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "C_S_AXI_HIGHADDR" -parent ${Page_0}
+  #Adding Group
+  set MCS-4_Customization [ipgui::add_group $IPINST -name "MCS-4 Customization" -parent ${Page_0}]
+  set_property tooltip {MCS-4 Customization} ${MCS-4_Customization}
+  set NUM_ROMS [ipgui::add_param $IPINST -name "NUM_ROMS" -parent ${MCS-4_Customization}]
+  set_property tooltip {Number of 4001 ROMs to instantiate} ${NUM_ROMS}
+  set NUM_RAMS [ipgui::add_param $IPINST -name "NUM_RAMS" -parent ${MCS-4_Customization}]
+  set_property tooltip {Number of 4002 RAMs} ${NUM_RAMS}
+  set NUM_RAM_ROWS [ipgui::add_param $IPINST -name "NUM_RAM_ROWS" -parent ${MCS-4_Customization}]
+  set_property tooltip {Number of rows of 4002 RAMs} ${NUM_RAM_ROWS}
+  set NUM_RAM_COLS [ipgui::add_param $IPINST -name "NUM_RAM_COLS" -parent ${MCS-4_Customization}]
+  set_property tooltip {Number of columns of 4002 RAMs} ${NUM_RAM_COLS}
+
+
+
+}
+
+proc update_PARAM_VALUE.NUM_RAMS { PARAM_VALUE.NUM_RAMS PARAM_VALUE.NUM_RAM_ROWS PARAM_VALUE.NUM_RAM_COLS } {
+	# Procedure called to update NUM_RAMS when any of the dependent parameters in the arguments change
+	
+	set NUM_RAMS ${PARAM_VALUE.NUM_RAMS}
+	set NUM_RAM_ROWS ${PARAM_VALUE.NUM_RAM_ROWS}
+	set NUM_RAM_COLS ${PARAM_VALUE.NUM_RAM_COLS}
+	set values(NUM_RAM_ROWS) [get_property value $NUM_RAM_ROWS]
+	set values(NUM_RAM_COLS) [get_property value $NUM_RAM_COLS]
+	set_property value [gen_USERPARAMETER_NUM_RAMS_VALUE $values(NUM_RAM_ROWS) $values(NUM_RAM_COLS)] $NUM_RAMS
+}
+
+proc validate_PARAM_VALUE.NUM_RAMS { PARAM_VALUE.NUM_RAMS } {
+	# Procedure called to validate NUM_RAMS
+	return true
+}
+
+proc update_PARAM_VALUE.NUM_RAM_COLS { PARAM_VALUE.NUM_RAM_COLS } {
+	# Procedure called to update NUM_RAM_COLS when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.NUM_RAM_COLS { PARAM_VALUE.NUM_RAM_COLS } {
+	# Procedure called to validate NUM_RAM_COLS
+	return true
+}
+
+proc update_PARAM_VALUE.NUM_RAM_ROWS { PARAM_VALUE.NUM_RAM_ROWS } {
+	# Procedure called to update NUM_RAM_ROWS when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.NUM_RAM_ROWS { PARAM_VALUE.NUM_RAM_ROWS } {
+	# Procedure called to validate NUM_RAM_ROWS
+	return true
+}
+
+proc update_PARAM_VALUE.NUM_ROMS { PARAM_VALUE.NUM_ROMS } {
+	# Procedure called to update NUM_ROMS when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.NUM_ROMS { PARAM_VALUE.NUM_ROMS } {
+	# Procedure called to validate NUM_ROMS
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_ID_WIDTH { PARAM_VALUE.C_S_AXI_ID_WIDTH } {
+	# Procedure called to update C_S_AXI_ID_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_ID_WIDTH { PARAM_VALUE.C_S_AXI_ID_WIDTH } {
+	# Procedure called to validate C_S_AXI_ID_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_DATA_WIDTH { PARAM_VALUE.C_S_AXI_DATA_WIDTH } {
+	# Procedure called to update C_S_AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_DATA_WIDTH { PARAM_VALUE.C_S_AXI_DATA_WIDTH } {
+	# Procedure called to validate C_S_AXI_DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_ADDR_WIDTH { PARAM_VALUE.C_S_AXI_ADDR_WIDTH } {
+	# Procedure called to update C_S_AXI_ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_ADDR_WIDTH { PARAM_VALUE.C_S_AXI_ADDR_WIDTH } {
+	# Procedure called to validate C_S_AXI_ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_AWUSER_WIDTH { PARAM_VALUE.C_S_AXI_AWUSER_WIDTH } {
+	# Procedure called to update C_S_AXI_AWUSER_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_AWUSER_WIDTH { PARAM_VALUE.C_S_AXI_AWUSER_WIDTH } {
+	# Procedure called to validate C_S_AXI_AWUSER_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_ARUSER_WIDTH { PARAM_VALUE.C_S_AXI_ARUSER_WIDTH } {
+	# Procedure called to update C_S_AXI_ARUSER_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_ARUSER_WIDTH { PARAM_VALUE.C_S_AXI_ARUSER_WIDTH } {
+	# Procedure called to validate C_S_AXI_ARUSER_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_WUSER_WIDTH { PARAM_VALUE.C_S_AXI_WUSER_WIDTH } {
+	# Procedure called to update C_S_AXI_WUSER_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_WUSER_WIDTH { PARAM_VALUE.C_S_AXI_WUSER_WIDTH } {
+	# Procedure called to validate C_S_AXI_WUSER_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_RUSER_WIDTH { PARAM_VALUE.C_S_AXI_RUSER_WIDTH } {
+	# Procedure called to update C_S_AXI_RUSER_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_RUSER_WIDTH { PARAM_VALUE.C_S_AXI_RUSER_WIDTH } {
+	# Procedure called to validate C_S_AXI_RUSER_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_BUSER_WIDTH { PARAM_VALUE.C_S_AXI_BUSER_WIDTH } {
+	# Procedure called to update C_S_AXI_BUSER_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_BUSER_WIDTH { PARAM_VALUE.C_S_AXI_BUSER_WIDTH } {
+	# Procedure called to validate C_S_AXI_BUSER_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_BASEADDR { PARAM_VALUE.C_S_AXI_BASEADDR } {
+	# Procedure called to update C_S_AXI_BASEADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_BASEADDR { PARAM_VALUE.C_S_AXI_BASEADDR } {
+	# Procedure called to validate C_S_AXI_BASEADDR
+	return true
+}
+
+proc update_PARAM_VALUE.C_S_AXI_HIGHADDR { PARAM_VALUE.C_S_AXI_HIGHADDR } {
+	# Procedure called to update C_S_AXI_HIGHADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S_AXI_HIGHADDR { PARAM_VALUE.C_S_AXI_HIGHADDR } {
+	# Procedure called to validate C_S_AXI_HIGHADDR
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.C_S_AXI_ID_WIDTH { MODELPARAM_VALUE.C_S_AXI_ID_WIDTH PARAM_VALUE.C_S_AXI_ID_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_ID_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_ID_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_DATA_WIDTH { MODELPARAM_VALUE.C_S_AXI_DATA_WIDTH PARAM_VALUE.C_S_AXI_DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_ADDR_WIDTH { MODELPARAM_VALUE.C_S_AXI_ADDR_WIDTH PARAM_VALUE.C_S_AXI_ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_AWUSER_WIDTH { MODELPARAM_VALUE.C_S_AXI_AWUSER_WIDTH PARAM_VALUE.C_S_AXI_AWUSER_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_AWUSER_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_AWUSER_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_ARUSER_WIDTH { MODELPARAM_VALUE.C_S_AXI_ARUSER_WIDTH PARAM_VALUE.C_S_AXI_ARUSER_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_ARUSER_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_ARUSER_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_WUSER_WIDTH { MODELPARAM_VALUE.C_S_AXI_WUSER_WIDTH PARAM_VALUE.C_S_AXI_WUSER_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_WUSER_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_WUSER_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_RUSER_WIDTH { MODELPARAM_VALUE.C_S_AXI_RUSER_WIDTH PARAM_VALUE.C_S_AXI_RUSER_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_RUSER_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_RUSER_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S_AXI_BUSER_WIDTH { MODELPARAM_VALUE.C_S_AXI_BUSER_WIDTH PARAM_VALUE.C_S_AXI_BUSER_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S_AXI_BUSER_WIDTH}] ${MODELPARAM_VALUE.C_S_AXI_BUSER_WIDTH}
+}
+
