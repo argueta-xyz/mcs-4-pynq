a7593404a3d0bfa054f21bbea0b3558c552cd3e7
Alejandro Argueta <alejandro@argueta.xyz>
2019-08-27 00:14:43 -0700

    Move IP Repo to vivado/

--- vivado/mcs4pynq_proj.tcl
+++ vivado/mcs4pynq_proj.tcl
@@ -170,7 +170,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 
 # Set IP repository paths
 set obj [get_filesets sources_1]
-set_property "ip_repo_paths" "[file normalize "$origin_dir/../ip_repo/mcs4_1.0"]" $obj
+set_property "ip_repo_paths" "[file normalize "$origin_dir/ip_repo/mcs4_1.0"]" $obj
 
 # Rebuild user ip_repo's index before adding any source files
 update_ip_catalog -rebuild
